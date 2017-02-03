b7575f69e17b443ebb820192cdbc4f6425978155
global4g <global4glv@gmail.com>
2017-02-02 22:14:20 -0800

    Create Stack.java
    Effective Java #5 Eliminate Obsolete reference

---
+++ java-memory-leak/Stack.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class Stack {
+    private Object[] elements;
+    private int size = 0;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    public Stack() {
+        elements = new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    public void push(Object e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+
+    public Object pop() {
+        if (size == 0)
+            System.out.println("Stack Empty");
+        Object result = elements[--size];
+        return result;
+    }
+
+    /**
+     * Ensure space for at least one more element, roughly
+     * doubling the capacity each time the array needs to grow.
+     */
+    private void ensureCapacity() {
+        if (elements.length == size)
+            elements = Arrays.copyOf(elements, 2 * size + 1);
+    }
+}
+
+// Just before  line 21 add this to avoid Leak
+// elements[size] = null; //Eliminate Obsolete reference
